cf893ee663fd708b65de67a5409179c11b60adb4
sameh-Tawfiq <58302710+sameh-Tawfiq@users.noreply.github.com>
2022-03-26 14:58:10 +0200

    Update maint.tf

--- lab2/maint.tf
+++ lab2/maint.tf
@@ -3,7 +3,6 @@ provider "aws" {
 }
 resource "aws_instance" "vm" {
   ami           = "ami-0359b3157f016ae46"
-  subnet_id     = "subnet-b3af19cb"
   instance_type = "t2.micro"
   tags = {
     Name = "my-first-tf-node"
